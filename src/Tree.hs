df9b3b5564197d00c7538dac2147b314daa5c1b5
rcardin <riccardo.cardin@gmail.com>
2018-12-28 15:41:46 +0100

    Added some functions taken from 'Functional Programming in Scala'

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,6 +1,10 @@
 module Tree
 ( singleton
 , treeInsert
+, size
+, maximum'
+, depth
+, map'
 ) where
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
@@ -21,3 +25,33 @@ treeElem x (Node y left right)
     | x == y = True
     | x < y  = treeElem x left
     | x > y  = treeElem x right
+
+-- From "Functional Programming in Scala"
+-- Exercise 25
+-- Write a function size that counts the number of nodes in a tree.
+size :: Tree a -> Int
+size EmptyTree = 0
+size (Node _ left right) = 1 + size left + size right
+
+-- Exercise 26
+-- Write a function maximum that return the maximum element in a Tree[Int]
+maximum' :: (Ord a) => Tree a -> a
+maximum' EmptyTree = error "Error empty tree"
+maximum' (Node x EmptyTree EmptyTree) = x
+maximum' (Node x left EmptyTree) = maximum' left
+maximum' (Node x EmptyTree right) = maximum' right
+maximum' (Node x left right) = maximum [maximum' left, maximum' right]
+
+-- Exercise 27
+-- Write a function depth that returns the maximum path length from the
+-- root of a tree to any leaf
+depth :: Tree a -> Int
+depth EmptyTree = 0
+depth (Node _ leaf right) = maximum [depth leaf, depth right] + 1
+
+-- Exercise 28
+-- Write a function map, that modifies each element in a tree with
+-- a given function
+map' :: Tree a -> (a -> b) -> Tree b
+map' EmptyTree _ = EmptyTree
+map' (Node x left right) f = Node (f x) (map' left f) (map' right f)
